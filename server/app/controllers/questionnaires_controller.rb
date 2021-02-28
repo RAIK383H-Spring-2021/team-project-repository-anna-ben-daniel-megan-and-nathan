b51dd8748c1fce8cd1a30e88107211ff55ced26d
meganchaffey <mchaffey11@gmail.com>
2021-02-28 16:19:02 -0600

    comments

--- server/app/controllers/questionnaires_controller.rb
+++ server/app/controllers/questionnaires_controller.rb
@@ -1,7 +1,15 @@
 class QuestionnairesController < ApplicationController
   def index
+    #TODO: Get current questionnaire responses
+    respond_to do |format|
+      format.json { render json: { status: 'Questionnaires controller index received request' } }
+    end
   end
 
   def update
+    #TODO: Update questionnaire responses
+    respond_to do |format|
+      format.json { render json: { status: 'Questionnaires controller update received request' } }
+    end
   end
 end
