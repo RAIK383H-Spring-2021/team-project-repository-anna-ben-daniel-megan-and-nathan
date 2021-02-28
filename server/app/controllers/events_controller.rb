e06c430568941ec791006bf61c858bbccdf236c4
akrueger12 <anna.r.krueger1@gmail.com>
2021-02-28 16:21:33 -0600

    added todo:

--- server/app/controllers/events_controller.rb
+++ server/app/controllers/events_controller.rb
@@ -1,27 +1,27 @@
 class EventsController < ApplicationController
   def create
-    #Create a new event
+    #TODO: Create a new event
     respond_to do |format|
       format.json { render json: { status: '/events/create received request' } }
     end
   end
 
   def show
-    #Get a specific event's details
+    #TODO: Get a specific event's details
     respond_to do |format|
       format.json { render json: { status: '/events/show received request' } }
       end
   end
 
   def update
-    #update an event
+    #TODO: update an event
     respond_to do |format|
       format.json { render json: { status: '/events/update received request' } }
       end
   end
 
   def destroy
-    #delete an event
+    #TODO: delete an event
     respond_to do |format|
       format.json { render json: { status: '/events/destroy received request' } }
       end
