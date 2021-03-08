ca510db535e7a049b672454ba7cb1f0bedd5ac04
akrueger12 <anna.r.krueger1@gmail.com>
2021-03-07 19:48:54 -0600

    fleshed out invitees controller

--- server/app/controllers/invitees_controller.rb
+++ server/app/controllers/invitees_controller.rb
@@ -1,15 +1,25 @@
 class InviteesController < ApplicationController
   def index
     #TODO: get list of invitees
+    @part = Participant.where(event_id: params[:event_id]).collect(&:user_id)
+    @invitees = User.find(@part)
+
     respond_to do |format|
-      format.json { render json: { status: 'invitees/index received request' } }
+      format.json { render json: @invitees.as_json }
       end
   end
 
   def create
     #TODO: add invitee to an event
+    @invitee = Participant.new(
+      user_id: params[:user_id],
+      event_id: params[:event_id],
+      questionnaire_complete: params[:questionnaire_complete],
+    )
+    @invitee.save!
+
     respond_to do |format|
-      format.json { render json: { status: 'invitees/create received request' } }
+      format.json { render json: { id: @invitee[:id] } }
       end
   end
 end
