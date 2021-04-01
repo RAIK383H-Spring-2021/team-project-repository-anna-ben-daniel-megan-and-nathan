5b24d4024d459b16ff7b75c56986413bf1eb6e34
Ben Jammin <blohrman@huskers.unl.edu>
2021-04-01 17:36:01 -0500

    added test db seeding for rspec

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,68 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# NOTE: We are utilizing this for TESTING PURPOSES ONLY. This seed is loaded in when the RSpec is run.
+
+# seed two users with questionnaires
+@quest = Questionnaire.create()
+@user = User.create(email: "test@test.test", 
+  first_name: "first", 
+  last_name: "last",
+  password: "password",
+  password_confirmation: "password",
+  questionnaire_id: @quest.id,
+  privacy_level: 1
+)
+
+@invited_quest = Questionnaire.create()
+@invited_user = User.create(email: "hello@hello.hello", 
+  first_name: "first", 
+  last_name: "last",
+  password: "password",
+  password_confirmation: "password",
+  questionnaire_id: @invited_quest.id,
+  privacy_level: 1
+)
+
+# seed two events, one hosted by each
+@event1 = Event.create(
+  title: "test",
+  host_id: @user.id,
+  description: "test event",
+  date_time: Time.now,
+  food_prepackaged: true,
+  food_buffet: false,
+  location: "test",
+  indoor: true,
+  outdoor: false,
+  remote: false,
+  score: 1
+)
+
+@event2 = Event.create(
+  title: "invited test",
+  host_id: @invited_user.id,
+  description: "test event",
+  date_time: Time.now,
+  food_prepackaged: true,
+  food_buffet: false,
+  location: "test",
+  indoor: true,
+  outdoor: false,
+  remote: false,
+  score: 1
+)
+
+# seed two invitations, each user invited to the other event
+Participant.create(
+  user_id: @user.id,
+  event_id: @event2.id,
+  questionnaire_complete: 1
+)
+
+Participant.create(
+  user_id: @invited_user.id,
+  event_id: @event1.id,
+  questionnaire_complete: 1
+)
--- server/spec/requests/users_controller_spec.rb
+++ server/spec/requests/users_controller_spec.rb
@@ -2,31 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "user controller requests", type: :request do
   before do
-    # create dummy user data (used for authentication purposes and event creation)
-    @quest = Questionnaire.create()
-    @user = User.create(email: "test@test.test", 
-      first_name: "first", 
-      last_name: "last",
-      password: "password",
-      password_confirmation: "password",
-      questionnaire_id: @quest.id,
-      privacy_level: 1
-    )
-
-    @invited_quest = Questionnaire.create()
-    @invited_user = User.create(email: "hello@hello.hello", 
-      first_name: "first", 
-      last_name: "last",
-      password: "password",
-      password_confirmation: "password",
-      questionnaire_id: @invited_quest.id,
-      privacy_level: 1
-    )
-
     # get token for authentication purposes
     post '/users/login', params: { email: "test@test.test", password: "password" }
     @res = JSON.parse(response.body)
     @token = @res["token"]
+
+    # get user corresponding to above auth token
+    @user = User.find_by(email: "test@test.test")
   end
 
   describe "GET #index" do
@@ -69,43 +51,6 @@ RSpec.describe "user controller requests", type: :request do
   end
 
   describe "GET #show" do
-    before do
-      Event.create(
-        title: "test",
-        host_id: @user.id,
-        description: "test event",
-        date_time: Time.now,
-        food_prepackaged: true,
-        food_buffet: false,
-        location: "test",
-        indoor: true,
-        outdoor: false,
-        remote: false,
-        score: 1
-      )
-
-      @event = Event.create(
-        title: "invited test",
-        host_id: @invited_user.id,
-        description: "test event",
-        date_time: Time.now,
-        food_prepackaged: true,
-        food_buffet: false,
-        location: "test",
-        indoor: true,
-        outdoor: false,
-        remote: false,
-        score: 1
-      )
-
-      Participant.create(
-        user_id: @user.id,
-        event_id: @event.id,
-        questionnaire_complete: 1
-      )
-
-    end
-
     it "returns unauthorized http error if no auth token provided" do
       get "/users/#{@user.id}"
 
