faa47bbeee33c7bc896cd66fc1f19e2c95459d0c
Ben Jammin <blohrman@huskers.unl.edu>
2021-04-08 20:28:38 -0500

    added comfort metric module

---
+++ server/app/controllers/utils/comfort_metric.rb
@@ -0,0 +1,21 @@
+module ComfortMetric
+  def generateTotalScore
+    # TODO: everything
+  end
+
+  def generateLocationScore
+    # TODO: everything
+  end
+
+  def generateFoodScore
+    # TODO: everything
+  end
+
+  def generateMasksSocialDistancingScore
+    # TODO: everything
+  end
+
+  def generateGroupSizeScore
+    # TODO: everything
+  end
+end
